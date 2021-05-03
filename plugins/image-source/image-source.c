963fe59064b656a5712c491d9685c9a402beb886
Clayton Groeneveld <claytong1214@gmail.com>
2021-05-02 19:05:33 -0500

    image-source: Fix GIF images not rendering in studio mode
    In the preview pane, GIF images wouldn't render, unless also in
    the program pane.

--- plugins/image-source/image-source.c
+++ plugins/image-source/image-source.c
@@ -194,7 +194,7 @@ static void image_source_tick(void *data, float seconds)
 		}
 	}
 
-	if (obs_source_active(context->source)) {
+	if (obs_source_showing(context->source)) {
 		if (!context->active) {
 			if (context->if2.image.is_animated_gif)
 				context->last_time = frame_time;
